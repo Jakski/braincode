a30b04f40d7f39423112ffd355eb41e847123d8d
piemar1 <marcin-pieczynski@wp.pl>
2016-03-19 16:48:50 +0100

    Update db_conn.py
    last zmiany Marcin

--- backend/app/db_conn.py
+++ backend/app/db_conn.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 import pg8000
 
-conn = pg8000.connect(host='51.254.113.72', user="braincode", password="brainfuck", database='driny', socket_timeout=100, port=5432)
+conn = pg8000.connect(host='51.254.113.72', user="braincode", password="brainfuck",
+                      database='driny', socket_timeout=100, port=5432)
 
 
 def selectFrom(sqlScript):
@@ -30,6 +32,8 @@ def dbSelectTool(id):
     return selectFrom(sqlScript)
 
 
+
+
 def dbInsertType(name):
     sqlScript = "INSERT INTO type(name) VALUES ('{a}');".format(a=name)
     insertInto(sqlScript)
@@ -43,6 +47,8 @@ def dbSelectType(id, name):
     else:
         sqlScript = "select * from type where id_type = '{a}'".format(a=id)
     return selectFrom(sqlScript)
+
+
 
 
 def dbInsertHash(hash):
@@ -60,8 +66,10 @@ def dbSelectHash(id, hash):
     return selectFrom(sqlScript)
 
 
+
+
 def dbInserAlco(id_hash, id_type, name, manufacturer):
-    sqlScript = "INSERT INTO alco(id_hash, id_type, name, manufacturer) VALUES('{a}', '{b}', '{c}', '{d}'');".format(a=id_hash, b=id_type, c=name, d=manufacturer)
+    sqlScript = "INSERT INTO alco(id_hash, id_type, name, manufacturer) VALUES('{a}', '{b}', '{c}', '{d}');".format(a=id_hash, b=id_type, c=name, d=manufacturer)
     insertInto(sqlScript)
 
 
@@ -73,6 +81,8 @@ def dbSelectAlco(id_hash, id_type):
     else:
         sqlScript = "select * from alco where id_hash = '{a}'".format(a=id_hash)
     return selectFrom(sqlScript)
+
+
 
 
 def dbInsertIngridient(name, description):
@@ -90,15 +100,85 @@ def dbSelectIngridient(id_igridient, name):
     return selectFrom(sqlScript)
 
 
-def dbInsertRecipe(id_usr,title,description,tab_ingridiends,tab_ingridiends_quantity, tab_tools):
-    sqlScript = "INSERT INTO recipe(id_usr, title, description, tab_ingridiends, tab_ingridiends_quantity, tab_tools)VALUES ('{a}', '{b}', '{c}',ARRAY[{d}], ARRAY[{e}], ARRAY[{f}]);".format(a=id_usr, b=title, c=description, d=tab_ingridiends, e=tab_ingridiends_quantity, f=tab_tools )
-    insertInto(sqlScript)
 
+
+def dbInsertRecipe(id_usr, title,description, tab_ingridiends, tab_ingridiends_quantity, tab_tools):
+    sqlScript = "INSERT INTO recipe(id_usr, title, description, tab_ingridiends, tab_ingridiends_quantity, tab_tools)VALUES ('{a}', '{b}', '{c}',ARRAY{d}, ARRAY{e}, ARRAY{f});".format(a=id_usr, b=title, c=description, d=tab_ingridiends, e=tab_ingridiends_quantity, f=tab_tools)
+    insertInto(sqlScript)
 
 def dbSelectRecipe():
     sqlScript = "select * from recipe "
     return selectFrom(sqlScript)
 
+
 if __name__ == "__main__":
-    print(dbSelectType("0", "0"))
-    print('stop')
+
+    # dbInsertRecipe(1, "Martini, wstrząśnięte, nie zmieszane",
+    #                "100 ml Martini Bianco + plaste cytryny + odrobina skruszonego lodu."
+    #                "Połączyć składniki, zamieszać", [3,5,6], [1, 200, 1], [7, 5])
+
+    # dbInsertIngridient(name, description)
+    # dbInsertIngridient("Cola", "żródło kofeiny i cukru")
+    # dbInsertIngridient("cytrna", "żródło kwasoty")
+    # dbInsertIngridient("vodka", " źródło procentów")
+    # dbInsertIngridient("martini", "dla smaku")
+    # dbInsertIngridient("lód", "dla ochłody")
+    # dbInsertIngridient("sok pomarańczony", "dla smaku")
+    # dbInsertIngridient("Johny Walker", "źródło procentów i smaku")
+
+    # dbInsertIngridient("sok pożeczkowy", "dla koloru")
+    # dbInsertIngridient("sok bananowy", "do smaku")
+
+    # dbInserAlco(id_hash, id_type, name, manufacturer)
+    # dbInserAlco(2, 9, 'coca-cola', 'Coca-Cola')
+    # dbInserAlco(3, 10, 'cytryna zółta', 'Nature')
+    # dbInserAlco(4, 9, 'ice', 'Nature')
+    # dbInserAlco(5, 3, 'Martini Bianco', 'babcia')
+    # dbInserAlco(6, 11, 'shaker manualny', 'Tłocznia metali')
+    # dbInserAlco(7, 9, 'sok porzeczkowy', 'Tymbark')
+    # dbInserAlco(8, 12, 'Johny Walker', 'Johny Walker')
+    # dbInserAlco(9, 3, 'Carlo Rossi', 'Carlo Rossi')
+    # dbInserAlco(10, 6, 'Wyborowa', 'Wyborowa SA')
+
+    # dbInsertType("Non-alkohol")
+    # dbInsertType("Owoc")
+    # dbInsertType("sprzęt")
+    # dbInsertType("Whiskey")
+
+    #2  dbInsertHash('cola.png')
+    #3  dbInsertHash('cytryna.png')
+    #4  dbInsertHash('lód.png')
+    #5  dbInsertHash('martini.png')
+    #6  dbInsertHash('shacker.png')
+    #7  dbInsertHash('sok.png')
+    #8  dbInsertHash('whisky.png')
+    #9  dbInsertHash('wino.png')
+    #10 dbInsertHash('wodka.png')
+
+    print("\nTYPE !!!!!")
+    for elem in dbSelectTool("0"):
+        print(elem)
+
+    print("HASH !!!!!")
+    for elem in dbSelectHash("0", "0"):
+        print(elem)
+
+    print("\nAlco !!!!!")
+    for elem in dbSelectAlco("0", "0"):
+        print(elem)
+
+    print("\nTYPE !!!!!")
+    for elem in dbSelectType("0", "0"):
+        print(elem)
+
+    print("\nIngredient !!!!!")
+    for elem in dbSelectIngridient("0", "0"):
+        print(elem)
+
+    print("\nPrzepisy !!!!!!!!!")
+    for elem in dbSelectRecipe():
+        print(elem)
+
+
+
+
