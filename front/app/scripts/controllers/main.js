e5b2c4762200a8f109da375f5ea917aed6faba63
Rayvenden <rayunderwater@gmail.com>
2016-03-19 11:27:09 +0100

    Add images

--- front/app/scripts/controllers/main.js
+++ front/app/scripts/controllers/main.js
@@ -8,10 +8,17 @@
  * Controller of the frontApp
  */
 angular.module('frontApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.selected = [];
+    $scope.pickItem = function (item) {
+        if ($scope.selected.indexOf(item.originalEvent.target.getAttribute('src')) ) {
+            $scope.selected.push(item.originalEvent.target.getAttribute('src'));
+            console.log($scope.selected);
+        }
+    }
   });
